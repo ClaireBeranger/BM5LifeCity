799a22441009c716e2a8fe08bd9ae797d5ae18be
Chri- <Chri-@LAPTOP-D3LVSR5A>
2019-02-01 15:45:22 +0100

    create package

---
+++ src/gui/MainGui.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package gui;
+
+/**
+ * @author Chri-
+ *
+ */
+public class MainGui {
+
+	/**
+	 * 
+	 */
+	public MainGui() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
