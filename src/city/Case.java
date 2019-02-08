bf02f49e304f1bcd08e4b0010dffc713be3825ee
Fares METIDJI <faresinside@gmail.com>
2019-02-08 15:58:04 +0100

    test

--- src/city/Case.java
+++ src/city/Case.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package city;
 
 import java.awt.Graphics;
@@ -43,3 +44,49 @@ public class Case {
 	
 
 }
+=======
+package city;
+
+
+import java.awt.Graphics;
+
+
+public class Case {
+
+
+	private int noCase;
+	private int x;
+	private int y;
+
+	public enum type {HOME, STATION, RES_BUILDING, ADMIN_BUILDING, COM_BUILDING} ;  
+	
+	public Case() {
+		//hello faress oumsss
+	}
+		
+
+	public int getNoCase() {
+		return noCase;
+	}
+
+	public void setNoCase(int noCase) {
+		this.noCase = noCase;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+}
+>>>>>>> branch 'master' of https://github.com/ClaireBeranger/BM5LifeCity
