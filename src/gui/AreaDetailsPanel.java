9c72e8af6e7937ea8a0677eb4cc032c7182e76a4
Chri- <Chri-@LAPTOP-D3LVSR5A>
2019-02-08 15:31:37 +0100

    ajout classe information quartier et modification classes ihm

---
+++ src/gui/AreaDetailsPanel.java
@@ -0,0 +1,54 @@
+package gui;
+
+import javax.swing.JPanel;
+/**
+ * 
+ * @author Chri-
+ *
+ */
+public class AreaDetailsPanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -521658874105155401L;
+	
+    public AreaDetailsPanel() {
+        initComponents();
+    }
+    
+    private void initComponents() {
+    	
+    	titlejlabel = new javax.swing.JLabel();
+    	
+    	titlejlabel.setFont(new java.awt.Font("Silom", 1, 18));
+    	titlejlabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+    	titlejlabel.setText("Information du quartier");
+
+      //  ValidatejButton1.setText("Valider");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(titlejlabel)
+                .addGap(30, 30, 30)
+                .addContainerGap(15, Short.MAX_VALUE))
+        );
+    }
+    
+   /* public static void main(String args[]) {
+    	 
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new AreaDetailsPanel().setVisible(true);
+            }
+        });
+    }*/
+    
+    private javax.swing.JLabel titlejlabel;
+
+}
--- src/gui/MapDescription.java
+++ src/gui/MapDescription.java
@@ -1,5 +1,21 @@
 package gui;
 
-public class MapDescription {
+import java.awt.BasicStroke;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+public class MapDescription extends JPanel{
+	
+	
 
 }
