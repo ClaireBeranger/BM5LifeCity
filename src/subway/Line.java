7bce46a893c4cc80fc8d7be88296b64a634c679d
Oumaima <Oumaima@10.77.86.68>
2019-02-08 15:51:29 +0100

    Class Line and Station

--- src/subway/Line.java
+++ src/subway/Line.java
@@ -1,4 +1,23 @@
 package subway;
 
+import java.util.ArrayList;
+
 public class Line {
+	
+	private String nameLine;
+	private ArrayList stations = new ArrayList<Station>();
+	private int timeTraject;
+	
+	
+	public Line(String nameLine) {
+		super();
+		this.nameLine = nameLine;
+	} 
+	
+	
+	public void travelFromTo(Station source, Station dest ) {
+		
+		
+	}
+	
 }
--- src/subway/Station.java
+++ src/subway/Station.java
@@ -1,4 +1,40 @@
 package subway;
 
+import city.Districts;
+import java.util.ArrayList;
+
 public class Station {
+
+	private String nameStation;
+	private ArrayList<Districts> districtRelied = new ArrayList<Districts>();
+	
+	
+	
+	public Station(String nameStation) {
+		super();
+		this.nameStation = nameStation;
+	}
+
+
+
+	public String getNameStation() {
+		return nameStation;
+	}
+
+	public void setNameStation(String nameStation) {
+		this.nameStation = nameStation;
+	}
+
+	public ArrayList<Districts> getDistrictRelied() {
+		return districtRelied;
+	}
+	
+	public void setDistrictRelied(ArrayList<Districts> districtRelied) {
+		this.districtRelied = districtRelied;
+	} 
+	
+	
+	
+	
+
 }
