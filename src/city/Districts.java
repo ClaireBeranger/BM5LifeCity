ce76d7e45e863ac1275428639605c1149d5cea88
Oumaima <Oumaima@10.77.86.68>
2019-02-22 15:45:21 +0100

    hihi

--- src/city/Districts.java
+++ src/city/Districts.java
@@ -4,16 +4,35 @@ public interface Districts {
 
 	
 	public void initDistrict() ;
+
 	
 	
-	public String getNameDsitrict() ;
-	
-	public void setNameDsitrict(String nameDsitrict);
+	public String getNameDsitrict();
+	public void setNameDsitrict(String nameDsitrict) ;
 	
 	public String getTypeDistrict() ;
-	
 	public void setTypeDistrict(String typeDistrict) ;
+
+	public Case[][] getMap() ;
+	public void setMap(Case[][] map) ;
+
+	public int getPrice() ;
+	public void setPrice(int price) ;
 	
+	public int getLevel() ;
+	public void setLevel(int level);
+
+	public int getCoordX() ;
+	public void setCoordX(int coordX) ;
+
+	public int getCoordY() ;
+
+	public void setCoordY(int coordY) ;
+
+	public boolean isStation() ;
+	public void setStation(boolean station);
+
+	public boolean isAccessibility() ;
 	public void setAccessibility(boolean accessibility);
 	
 	@Override
