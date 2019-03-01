2c718d568ba60a59e4f73e228f89024574b3d69a
Oumaima <Oumaima@10.77.86.68>
2019-03-01 15:39:14 +0100

    clock

--- src/clock/Clock.java
+++ src/clock/Clock.java
@@ -129,11 +129,13 @@ public class Clock {	//creation of the clock
 		}
 	}
 
-	public String toString() {
-		return "HOUR : " + getHour() + ":" + getMinute() + " ; DATE : " + getDay() + "/" + getMonth() + "/" + getYear();
+	public String toStringHour() {
+		return getHour() + ":" + getMinute() ;
 	}
 	
-
+	public String toStringDate() {
+		return  getDay() + "/" + getMonth() + "/" + getYear() ;
+	}
 	
 	
 }
